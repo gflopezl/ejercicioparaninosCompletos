e37903db4a72b964995519022d8ba99da591794c
iplacex-cine-api-gabriela_lopez <gflopezl@alumno.iplacex.cl>
2025-07-20 15:16:07 -0400

    Arreglos en usuarioController y usuarioRoutes

--- backend/routes/usuarioRoutes.js
+++ backend/routes/usuarioRoutes.js
@@ -10,19 +10,10 @@ const {
 const verificarToken = require('../middleware/verificarToken');
 const verificarAdmin = require('../middleware/verificarAdmin');
 
-// Verificamos que todas las importaciones sean funciones
-console.log('registrarUsuario:', typeof registrarUsuario);
-console.log('iniciarSesion:', typeof iniciarSesion);
-console.log('obtenerFechaInscripcion:', typeof obtenerFechaInscripcion);
-console.log('verificarToken:', typeof verificarToken);
-console.log('verificarAdmin:', typeof verificarAdmin);
-
-// Público
 router.post('/registro', registrarUsuario);
 router.post('/login', iniciarSesion);
 router.get('/fecha-inscripcion/:id', obtenerFechaInscripcion);
 
-// Protegido para admins
 router.post('/admin/crear-usuario', verificarToken, verificarAdmin, registrarUsuario);
 
 module.exports = router;
