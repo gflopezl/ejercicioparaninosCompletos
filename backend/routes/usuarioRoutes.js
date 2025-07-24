671220ee8084dd871b3c0e0dcb77f2505d57eec7
iplacex-cine-api-gabriela_lopez <gflopezl@alumno.iplacex.cl>
2025-07-24 17:40:58 -0400

    Corrige importación del modelo usuario en usuarioRoutes.js

--- backend/routes/usuarioRoutes.js
+++ backend/routes/usuarioRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Usuario = require('../models/Usuario'); // modelo mongoose
+const Usuario = require('../models/usuario'); // modelo mongoose
 
 // Obtener todos los usuarios
 router.get('/', async (req, res) => {
